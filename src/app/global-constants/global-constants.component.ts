2c96986fc35c2054ac4ee6f0f58350f03160e3d1
Ko Brad <javarako@gmail.com>
2021-06-16 21:42:43 -0400

    fix layout

--- src/app/global-constants/global-constants.component.ts
+++ src/app/global-constants/global-constants.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GlobalConstantsComponent implements OnInit {
 
-  public static apiURL: string = "http://ec2-34-239-249-199.compute-1.amazonaws.com:8081";
+  public static apiURL: string = "http://ec2-3-89-160-132.compute-1.amazonaws.com:8081";
   //public static apiURL: string = "http://localhost:8081";
 
   constructor() { }
